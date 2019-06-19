d1799e8cd3459814f9daeab7715a3150a6388660
Cristian Necula <virusz@gmail.com>
2019-06-19 09:28:55 +0300

    (wip) Should be defined as an observer function within the property declaration.

--- cosmoz-moment-timeago.js
+++ cosmoz-moment-timeago.js
@@ -20,12 +20,6 @@ class CosmozMomentTimeago extends momentAware(PolymerElement) {
 		return 'cosmoz-moment-timeago';
 	}
 
-	static get observers() {
-		return [
-			'refreshIntervalChanged(refreshInterval)'
-		];
-	}
-
 	static get properties() {
 		return {
 			date: {
@@ -38,7 +32,12 @@ class CosmozMomentTimeago extends momentAware(PolymerElement) {
 			},
 			refreshInterval: {
 				type: Number,
-				value: 60000
+				value: 60000,
+				observer(refreshInterval) {
+					this.set('_intervalId', window.setInterval(() => {
+						this._kicker += 1;
+					}, refreshInterval));
+				}
 			},
 			_kicker: {
 				type: Number,
@@ -55,12 +54,6 @@ class CosmozMomentTimeago extends momentAware(PolymerElement) {
 	disconnectedCallback() {
 		super.disconnectedCallback();
 		window.clearInterval(this._intervalId);
-	}
-
-	refreshIntervalChanged(refreshInterval) {
-		this.set('_intervalId', window.setInterval(() => {
-			this._kicker += 1;
-		}, refreshInterval));
 	}
 }
 customElements.define(CosmozMomentTimeago.is, CosmozMomentTimeago);
